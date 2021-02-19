038dee5e0ed119c794f431c3822806accb0d61ef
adnan morshed <adnan.starit118@gmail.com>
2021-02-19 16:39:51 +0600

    what's up UI added

--- lib/signin_phone_number_choose_verification.dart
+++ lib/signin_phone_number_choose_verification.dart
@@ -8,7 +8,7 @@ import 'package:otp/signin_phone_number_verification.dart';
 class SigninPhoneNumberChooseVerificationPage extends StatefulWidget {
   final String phoneNumber;
 
-  const SigninPhoneNumberChooseVerificationPage({Key key, this.phoneNumber = ''}) : super(key: key);
+  const SigninPhoneNumberChooseVerificationPage({Key key, this.phoneNumber = '01831679008'}) : super(key: key);
 
   @override
   _SigninPhoneNumberChooseVerificationPageState createState() => _SigninPhoneNumberChooseVerificationPageState();
@@ -81,43 +81,43 @@ class _SigninPhoneNumberChooseVerificationPageState extends State<SigninPhoneNum
             ),
             ///what's up  todo
 
-            // GestureDetector(
-            //   onTap: (){
-            //     Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SigninPhoneNumberVerificationPage(verificationType: 'wa', phoneNumber: widget.phoneNumber)));
-            //   },
-            //   child: Card(
-            //     shape: RoundedRectangleBorder(
-            //       borderRadius: BorderRadius.circular(10),
-            //     ),
-            //     elevation: 1,
-            //     color: Colors.white,
-            //     child: Container(
-            //       margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
-            //       width: MediaQuery.of(context).size.width - 60,
-            //       child: Row(
-            //         children: [
-            //           Image(image: AssetImage("assets/images/whatsapp.png"), width: 50, height: 50),
-            //           Container(
-            //             margin: EdgeInsets.only(left: 10),
-            //             child: Column(
-            //               crossAxisAlignment: CrossAxisAlignment.start,
-            //               children: [
-            //                 Text(AppLocalizations.of(context).translate('whatsapp_to'), style: GlobalStyle.methodTitle),
-            //                 SizedBox(
-            //                   height: 3,
-            //                 ),
-            //                 Text(widget.phoneNumber, style: GlobalStyle.methodMessage)
-            //               ],
-            //             ),
-            //           )
-            //         ],
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // SizedBox(
-            //   height: 40,
-            // ),
+            GestureDetector(
+              onTap: (){
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SigninPhoneNumberVerificationPage(verificationType: 'wa', phoneNumber: widget.phoneNumber)));
+              },
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 1,
+                color: Colors.white,
+                child: Container(
+                  margin: EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  width: MediaQuery.of(context).size.width - 60,
+                  child: Row(
+                    children: [
+                      Icon(Icons.phone_android, color: Colors.green, size: 50),
+                      Container(
+                        margin: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('whatsapp_to'),
+                            SizedBox(
+                              height: 3,
+                            ),
+                            Text(widget.phoneNumber)
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
             Center(
               child: GestureDetector(
                 onTap: () {
